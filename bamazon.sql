4d87c1453b90bf5b05b97c9d02be5021f06cc65f
Elianice Gorniak <eligorniak@gmail.com>
2019-09-18 10:15:53 -0500

    updated with Challenge 1 done

--- bamazon.sql
+++ bamazon.sql
@@ -6,25 +6,60 @@ USE products_db;
 
 CREATE TABLE products
 (
-    item_id INT NOT NULL
-    AUTO_INCREMENT,
+    item_id INTEGER(30)
+    AUTO_INCREMENT NOT NULL,
     product_name VARCHAR
-    (100) NOT NULL,
+    (30),
     department_name VARCHAR
-    (100) NOT NULL,
+    (30),
     price DECIMAL
-    (6,2) NOT NULL,
-    stock_quantity INT NOT NULL,
+    (6,2),
+    stock_quantity INTEGER
+    (10),
     PRIMARY KEY
     (item_id)
 );
 
-
     INSERT INTO products
-        (product_name, department_name, price)
+        (product_name, department_name, price, stock_quantity)
     VALUES
-        ("vanilla", 2.50, 100);
-
+        ("Girl Backpack", "Fashion", 17.50, 35);
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantity)
+    VALUES
+        ("Samsumg Gallaxy Tablet", "Electronics", 279.99, 10);
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantity)
+    VALUES
+        ("Philips Shaver", "Beauty", 54.90, 42);
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantity)
+    VALUES
+        ("Bullova Watch", "Accessories", 197.50, 5);
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantity)
+    VALUES
+        ("JBL Headphones", "Electronics", 49.50, 12);
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantity)
+    VALUES
+        ("Javascript Guide", "Books", 37.97, 17);
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantity)
+    VALUES
+        ("Women's Flet Sandals", "Shoes", 17.50, 38);
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantity)
+    VALUES
+        ("Kitchen Weight Scale", "Home&Kitchen", 19.90, 26);
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantity)
+    VALUES
+        ("LEGO Toy Story", "Toys&Games", 111.99, 17);
+    INSERT INTO products
+        (product_name, department_name, price, stock_quantity)
+    VALUES
+        ("Women's Dress", "Fashion", 37.50, 22);
 
     SELECT *
-    from top_songs;
+    from products;
---
+++ bamazonCustomer.js
@@ -0,0 +1,88 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+// Variable the connection information for the sql db:
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "products_db"
+});
+
+// Connect to the mysql server and sql db:
+connection.connect(function(err) {
+  if (err) throw err;
+  // Run the showProducts function after the connection:
+  showTable();
+});
+
+// This function will show in the terminal the table products from the db:
+function showTable() {
+  connection.query(
+    "SELECT item_id, product_name, department_name, price FROM products",
+    function(err, rows) {
+      if (err) throw err;
+      console.log("");
+      console.log("========== BAMAZON STORE - SELECT PRODUCTS ==========");
+      console.log("ITEM     PRODUCT     DEPARTMENT     PRICE");
+      for (let i = 0; i < rows.length; i++) {
+        console.log(
+          rows[i].item_id +
+            "\t" +
+            rows[i].product_name +
+            "\t" +
+            rows[i].department_name +
+            "\t" +
+            rows[i].price
+        );
+      }
+      console.log("=====================================================");
+      questions();
+    }
+  );
+}
+
+// This function prompts the user for their purchase choices:
+function questions() {
+  inquirer
+    .prompt([
+      {
+        name: "itemId",
+        type: "input",
+        message: "Please, enter the ID of the product that you want to buy."
+      },
+      {
+        name: "quantity",
+        type: "input",
+        message: "Please, enter the quantity of the choosen product."
+      }
+    ])
+    .then(function(answer) {
+      connection.query(
+        "SELECT item_id, price, stock_quantity FROM products WHERE item_id = " +
+          answer.itemId,
+        function(err, row) {
+          if (err) throw err;
+          //console.log(parseInt(row[0].stock_quantity));
+          if (row[0].stock_quantity >= parseInt(answer.quantity)) {
+            connection.query(
+              "UPDATE products SET stock_quantity = stock_quantity - " +
+                answer.quantity +
+                " WHERE item_id = " +
+                answer.itemId
+            );
+            console.log(
+              "Your total purchase is US$ " +
+                row[0].price * parseInt(answer.quantity) +
+                "."
+            );
+          } else {
+            console.log("Insufficient quantity!");
+          }
+        }
+      );
+    });
+}
+
+// TODO: use connection.end();
